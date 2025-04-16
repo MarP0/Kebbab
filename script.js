ddc6eeedfb8773a2372192ddf97546cc165bc738
MarP0 <134074532+MarP0@users.noreply.github.com>
2025-04-16 12:54:37 +0000

    Pabeidzu LogIn.html (izņemot logo navbarā) un pielaboju Sūdzības lapu.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function sudziba() {
   alert("Sūdzība veiksmīgi nosūtīta!");
 }
+
+function ieiet() {
+  let Parole_ = document.getElementById("parole").value;
+  let E_pasts_ = document.getElementById("emails").value;
+  if (Parole_ == "12345678" && E_pasts_ == "admins@gmail.com")
+  {
+    alert("Veiksmīgi ielogošanās!");
+  }
+  else
+  {
+    alert("Nepareizs e-pasts vai parole");
+  }
+}
